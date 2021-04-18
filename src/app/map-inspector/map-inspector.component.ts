80fc7b0ac7ffdfc4ea1efe3979c3eb2a8b58bf96
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2021-04-18 18:42:45 +0200

    feature: Add button to download rendered maps

--- src/app/map-inspector/map-inspector.component.ts
+++ src/app/map-inspector/map-inspector.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnChanges, SimpleChanges, NgZone, ViewChild, ElementR
 import { TileSheet } from 'src/mighty-data/models/tile-sheet';
 import { Map } from 'src/mighty-data/models/map';
 import { ResourceManager } from 'src/mighty-data/resource-manager';
+import { SerializerService } from '../serializer.service';
 
 @Component({
   selector: 'app-map-inspector',
@@ -21,31 +22,18 @@ export class MapInspectorComponent implements OnChanges {
   @ViewChild('canvas')
   canvas: ElementRef<HTMLCanvasElement>;
   public context: CanvasRenderingContext2D;
-  constructor(private zone: NgZone) {}
+  constructor(private zone: NgZone, private serializer: SerializerService) {}
 
   ngOnChanges(changes: SimpleChanges) {
     if ('map' in changes) {
-      this.resourceManager.get(this.map.tilesheetSource).subscribe(
-        this.runHack(ts => {
-          this.tilesheet = ts.contents;
-          this.resourceManager.get(this.tilesheet.paletteSource).subscribe(
-            this.runHack(image => {
-              this.palette = image.contents.palette;
-              this.redraw();
-            })
-          );
-        })
-      );
+      this.redraw();
     }
 
     if (!this.context) {
       return;
     }
-    this.redraw();
-  }
 
-  private runHack(cb: (...args: any[]) => void): () => void {
-    return (...args: any[]) => this.zone.run(() => setTimeout(cb, 0, ...args));
+    this.redraw();
   }
 
   ngAfterViewInit(): void {
@@ -54,56 +42,12 @@ export class MapInspectorComponent implements OnChanges {
   }
 
   private redraw() {
-    const { palette, map, tilesheet } = this;
-    if (!palette || !map || !tilesheet) {
+    if (!this.map) {
       return;
     }
 
-    this.context.putImageData(this.draw(map), 0, 0);
-  }
-
-  draw(map: Map) {
-    console.log('draw');
-
-    const TileWidth = 32;
-    const TileHeight = 32;
-    const ZoneWidth = map.width;
-    const ZoneHeight = map.height;
-
-    const palette = this.palette;
-    const result = new ImageData(ZoneWidth * TileWidth, ZoneHeight * TileHeight);
-    const buffer = new ArrayBuffer(result.data.length);
-    const byteArray = new Uint8Array(buffer);
-    const data = new Uint32Array(buffer);
-    const bpr = ZoneWidth * TileWidth;
-
-    for (let y = 0; y < ZoneHeight; y++) {
-      for (let x = 0; x < ZoneWidth; x++) {
-        const tileId = map.tiles[y * ZoneWidth + x];
-        const pixels = this.tilesheet.tiles[tileId];
-        if (!pixels) {
-          console.log('Tile', tileId, 'of', this.tilesheet.tiles.length);
-        }
-        if (!pixels) {
-          continue;
-        }
-
-        const sy = y * TileHeight;
-        const sx = x * TileWidth;
-        let j = sy * bpr + sx;
-
-        for (let ty = 0; ty < TileHeight; ty++) {
-          for (let tx = 0; tx < TileWidth; tx++) {
-            const i = ty * TileWidth + tx;
-            data[j + tx] = palette[pixels[i]];
-          }
-
-          j += bpr;
-        }
-      }
-    }
-    result.data.set(byteArray);
-
-    return result;
+    this.serializer.drawMapImage(this.map).then(image => {
+      this.context.putImageData(image, 0, 0);
+    });
   }
 }
--- src/app/serializer.service.ts
+++ src/app/serializer.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { TileSheet, Image, ShapeSheet, ShapeSet, Shape } from 'src/mighty-data/models';
+import { TileSheet, Image, ShapeSheet, Map, Shape } from 'src/mighty-data/models';
 import { Resource } from 'src/mighty-data/resource';
 import { ResourceManager } from 'src/mighty-data/resource-manager';
 import { download, downloadImage } from 'src/util';
@@ -11,12 +11,20 @@ import JSZip from 'jszip';
 export class SerializerService {
   constructor(private resourceManager: ResourceManager) {}
 
+  public canSerialize(resource: Resource): boolean {
+    return resource.type === TileSheet || resource.type === ShapeSheet || resource.type === Map;
+  }
+
   public async download(resource: Resource): Promise<void> {
     if (resource.type === TileSheet) {
       await this.downloadTilesheet(resource);
     }
     if (resource.type === ShapeSheet) {
       await this.downloadShapeSheet(resource);
+    }
+
+    if (resource.type === Map) {
+      await this.downloadMapImage(resource);
     }
   }
 
@@ -113,7 +121,60 @@ export class SerializerService {
     return canvas.toDataURL('png').substr(22);
   }
 
-  public canSerialize(resource: Resource): boolean {
-    return resource.type === TileSheet || resource.type === ShapeSheet;
+  private async downloadMapImage(resource: Resource): Promise<void> {
+    const map = resource.contents as Map;
+    const image = await this.drawMapImage(map);
+    const name = resource.path.split('/')[1].split('.')[0];
+    const level = resource.path.split('-')[1];
+    downloadImage(image, `${name}-${level}-map.png`, 'png');
+  }
+
+  public async drawMapImage(map: Map): Promise<ImageData> {
+    const TileWidth = 32;
+    const TileHeight = 32;
+    const ZoneWidth = map.width;
+    const ZoneHeight = map.height;
+
+    const tileSheetResource = await this.resourceManager.get(map.tilesheetSource).toPromise();
+    const tileSheet = tileSheetResource.contents;
+
+    const paletteSourceImageResource = await this.resourceManager.get(tileSheet.paletteSource).toPromise();
+    const paletteSourceImage = paletteSourceImageResource.contents as Image;
+    const palette = paletteSourceImage.palette;
+
+    const result = new ImageData(ZoneWidth * TileWidth, ZoneHeight * TileHeight);
+    const buffer = new ArrayBuffer(result.data.length);
+    const byteArray = new Uint8Array(buffer);
+    const data = new Uint32Array(buffer);
+    const bpr = ZoneWidth * TileWidth;
+
+    for (let y = 0; y < ZoneHeight; y++) {
+      for (let x = 0; x < ZoneWidth; x++) {
+        const tileId = map.tiles[y * ZoneWidth + x];
+        const pixels = tileSheet.tiles[tileId];
+        if (!pixels) {
+          console.log('Tile', tileId, 'of', tileSheet.tiles.length);
+        }
+        if (!pixels) {
+          continue;
+        }
+
+        const sy = y * TileHeight;
+        const sx = x * TileWidth;
+        let j = sy * bpr + sx;
+
+        for (let ty = 0; ty < TileHeight; ty++) {
+          for (let tx = 0; tx < TileWidth; tx++) {
+            const i = ty * TileWidth + tx;
+            data[j + tx] = palette[pixels[i]];
+          }
+
+          j += bpr;
+        }
+      }
+    }
+    result.data.set(byteArray);
+
+    return result;
   }
 }
