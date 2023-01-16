2d332263327a48031e761b3374fc41cc83e9bb82
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2023-01-16 20:30:13 +0100

    fix: Adjust middleware to look for resource forks in `._`-prefixed files

--- resource-fork.config.js
+++ resource-fork.config.js
@@ -12,19 +12,27 @@ module.exports = {
             const fullPath = Path.resolve(Path.join(Path.resolve('./src'), path));
             const allowedPath = Path.resolve('./src');
 
+            // Make sure the requested path is within the project directory
             if (fullPath.indexOf(allowedPath) !== 0) {
               return;
             }
 
+            // Try to access resource fork via macOS naming convention
             let resourcePath = fullPath + '/..namedfork/rsrc';
             if (FS.existsSync(resourcePath)) {
               res.sendFile(resourcePath);
               return;
             }
 
-            const basename = '';
-            const filename = '';
-            resourcePath = basename + '/._' + filename;
+            const components = fullPath.split('/');
+            if (components.length <= 2) {
+              return;
+            }
+
+            // Try accessing resource fork by prepending `._` to the filename
+            const directory = components.slice(0, components.length - 2).join('/');
+            const filename = components[components.length - 1];
+            resourcePath = directory + '/._' + filename;
             if (FS.existsSync(resourcePath)) {
               res.sendFile(resourcePath);
               return;
